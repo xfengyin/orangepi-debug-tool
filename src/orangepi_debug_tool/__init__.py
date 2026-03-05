6d9802f2bce00de04cfa2fb87e113dffafe07fcf
xfengyin <55651142+xfengyin@users.noreply.github.com>
2026-03-05 18:10:18 +0800

    refactor: Create package init

---
+++ src/orangepi_debug_tool/__init__.py
@@ -0,0 +1,12 @@
+"""
+OrangePi Debug Tool - 现代化硬件调试工具
+
+一款专为 OrangePi 开发者设计的上位机调试工具
+"""
+
+__version__ = "2.0.0"
+__author__ = "xfengyin"
+
+from .app import OrangePiDebugTool
+
+__all__ = ["OrangePiDebugTool"]
